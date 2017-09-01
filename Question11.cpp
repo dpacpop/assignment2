204a6949b935aa40594562db3c6a0a3b407d45ac
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-01 17:03:59 +0530

    Create Question11.cpp

---
+++ Question11.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	double a=0,b=0,c=0,d=0;
+	cout<<"enter the principle, time and rate\n";
+	cin>>a>>b>>c;
+	d=a*b*c/100;
+	cout<<"the simple interest is:"<<d;
+	return 0;
+}
