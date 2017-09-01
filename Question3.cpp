e2b49b61f501170cf137ec6c782698aed223641a
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-01 10:13:43 +0530

    Create Question3.cpp

---
+++ Question3.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+# include<conio.h>
+using namespace std;
+int main()
+{
+	double a,b;
+	cout<<"enter the temperature in fahrenheit\n";
+	cin>>a;
+	b= (a-32)*5/9;
+	cout<<"the temperature in celcius is-"<<b<<endl;
+	return 0;
+}
