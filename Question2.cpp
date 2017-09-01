bdc49b5bd9fd5cd7b3d74ca8e6f5d589586f528a
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-09-01 10:14:14 +0530

    Update and rename question2.cpp to Question2.cpp

--- Question2.cpp
+++ Question2.cpp
@@ -5,7 +5,7 @@ int main()
     double a=0,b=0;
     cout<<"\n enter the temperature in celsius\n";
     cin>>a;
-    b=((a*9)/5)+32;
+    b=(a*9/5)+32;
     cout<<"\n temperature in fahrenheit =>"<<b;
     return 0;
 }
